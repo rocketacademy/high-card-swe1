c46af8b6921367c2f76a7696f9d3a9a094f9ffe1
Tinaes <aj.tinaes@gmail.com>
2021-07-12 22:19:21 +0800

    pre-class base complete

--- script.js
+++ script.js
@@ -1,1 +1,148 @@
-// Please implement exercise logic here
+let playersTurn = 1; // matches with starting instructions
+let player1Card;
+
+const player1Button = document.createElement('button');
+player1Button.className = 'button';
+const player2Button = document.createElement('button');
+player2Button.className = 'button';
+
+const gameInfo = document.createElement('div');
+gameInfo.className = 'output';
+
+const cardContainer = document.createElement('div');
+cardContainer.classList.add('card-container');
+document.body.appendChild(cardContainer);
+
+const deck = [];
+const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+const symbols = ['♥', '♦', '♣', '♠'];
+
+// Get a random index ranging from 0 (exclusive) to max (inclusive).
+const getRandomIndex = (max) => Math.ceil(Math.random() * max);
+
+const generateCard = (cardRank, suit) => {
+  let cardName = '';
+  let cardDisplay = '';
+  switch (cardRank) {
+    case 1:
+      cardName = 'Ace';
+      cardDisplay = 'A';
+      break;
+    case 11:
+      cardName = 'Jack';
+      cardDisplay = 'J';
+      break;
+    case 12:
+      cardName = 'Queen';
+      cardDisplay = 'Q';
+      break;
+    case 13:
+      cardName = 'King';
+      cardDisplay = 'K';
+      break;
+    default:
+      cardName = cardRank;
+      cardDisplay = cardRank;
+  }
+
+  const cardColor = suit < 2 ? 'red' : 'black';
+
+  const card = {
+    suitSymbol: symbols[suit],
+    suit: suits[suit],
+    name: cardName,
+    displayName: cardDisplay,
+    colour: cardColor,
+    rank: cardRank,
+    title: `${cardName} of ${suit}`,
+  };
+  // console.log(card);
+  return card;
+};
+
+// generate a shuffled standard deck of cards
+function makeDeck() {
+  for (let i = 1; i <= 13; i += 1) {
+    for (let j = 0; j < 4; j += 1) {
+      // generates cards and inserts randomly into deck
+      deck.splice(getRandomIndex(deck.length), 0, generateCard(i, j));
+    }
+  }
+}
+
+// Create a helper function for output to abstract complexity
+// of DOM manipulation away from game logic
+const output = (message) => {
+  gameInfo.innerText = message;
+};
+
+const player1Click = () => {
+  if (playersTurn === 1) {
+    player1Card = deck.pop();
+
+    const cardElement = printCard(player1Card);
+    // Empty cardContainer in case this is not the 1st round of gameplay
+    cardContainer.innerHTML = '';
+    // Append the card element to the card container
+    cardContainer.appendChild(cardElement);
+
+    playersTurn = 2;
+  }
+};
+
+const player2Click = () => {
+  if (playersTurn === 2) {
+    const player2Card = deck.pop();
+    const cardElement = printCard(player2Card);
+    // Append the card element to the card container
+    cardContainer.appendChild(cardElement);
+
+    playersTurn = 1;
+
+    if (player1Card.rank > player2Card.rank) {
+      output('player 1 wins');
+    } else if (player1Card.rank < player2Card.rank) {
+      output('player 2 wins');
+    } else {
+      output('tie');
+    }
+  }
+};
+
+const printCard = (cardInfo) => {
+  const suit = document.createElement('div');
+  suit.classList.add('suit', cardInfo.colour);
+  suit.innerText = cardInfo.suitSymbol;
+
+  const name = document.createElement('div');
+  name.classList.add('name', cardInfo.colour);
+  name.innerText = cardInfo.displayName;
+
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  card.appendChild(name);
+  card.appendChild(suit);
+
+  return card;
+};
+
+const initGame = () => {
+  makeDeck();
+
+  document.body.appendChild(gameInfo);
+  // initialize button functionality
+  player1Button.innerText = 'Player 1 Draw';
+  document.body.appendChild(player1Button);
+
+  player2Button.innerText = 'Player 2 Draw';
+  document.body.appendChild(player2Button);
+
+  player1Button.addEventListener('click', player1Click);
+  player2Button.addEventListener('click', player2Click);
+
+  // fill game info div with starting instructions
+  gameInfo.innerText = 'Its player 1 turn. Click to draw a card!';
+};
+
+initGame();
