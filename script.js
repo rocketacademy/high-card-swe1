16c585ba19bef3e683667f62e3cbe409c8159ee2
Tinaes <aj.tinaes@gmail.com>
2021-07-13 18:45:20 +0800

    variable draw with borders

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-let playersTurn = 1; // matches with starting instructions
-let player1Card;
-
-const player1Button = document.createElement('button');
-player1Button.className = 'button';
-const player2Button = document.createElement('button');
-player2Button.className = 'button';
-
-const gameInfo = document.createElement('div');
-gameInfo.className = 'output';
-
-const cardContainer = document.createElement('div');
-cardContainer.classList.add('card-container');
-document.body.appendChild(cardContainer);
+let playersTurn; // matches with starting instructions
+let player1Hand = [];
+let player2Hand = [];
+let scoreDiff;
 
 const deck = [];
 const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
 const symbols = ['♥', '♦', '♣', '♠'];
+
+const gameInfo = document.createElement('div');
+const cardContainer = document.createElement('div');
+const drawCount = document.createElement('input');
 
 // Get a random index ranging from 0 (inclusive) to max (exclusive).
 const getRandomIndex = (max) => Math.floor(Math.random() * max);
@@ -55,8 +49,8 @@ const generateCard = (cardRank, suit) => {
     colour: cardColor,
     rank: cardRank,
     title: `${cardName} of ${suit}`,
+    highLow: false,
   };
-  // console.log(card);
   return card;
 };
 
@@ -69,22 +63,40 @@ function makeDeck() {
     }
   }
 }
+const deal = () => deck.pop();
 
-// Create a helper function for output to abstract complexity
-// of DOM manipulation away from game logic
+const highLow = (playerHand) => {
+  const ranks = [];
+  for (let i = 0; i < playerHand.length; i += 1) {
+    ranks.push(playerHand[i].rank);
+  }
+  const highestCard = Math.max(...ranks);
+  const lowestCard = Math.min(...ranks);
+  playerHand[ranks.indexOf(highestCard)].highLow = true;
+  playerHand[ranks.indexOf(lowestCard)].highLow = true;
+  return highestCard - lowestCard;
+};
+
+// DOM output helper
 const output = (message) => {
   gameInfo.innerText = message;
 };
 
 const player1Click = () => {
   if (playersTurn === 1) {
-    player1Card = deck.pop();
-
-    const cardElement = printCard(player1Card);
-    // Empty cardContainer in case this is not the 1st round of gameplay
-    cardContainer.innerHTML = '';
-    // Append the card element to the card container
+    cardContainer.innerText = '';
+    player1Hand = [];
+    player2Hand = [];
+    scoreDiff = 0;
+    drawCount.disabled = true;
+    for (let i = 0; i < drawCount.value; i += 1) {
+      const card = deal();
+      player1Hand.push(card);
+    }
+    scoreDiff += highLow(player1Hand);
+    const cardElement = printHand(player1Hand);
     cardContainer.appendChild(cardElement);
+
     output('Player 2\'s turn!');
     playersTurn = 2;
   }
@@ -92,21 +104,34 @@ const player1Click = () => {
 
 const player2Click = () => {
   if (playersTurn === 2) {
-    const player2Card = deck.pop();
-    const cardElement = printCard(player2Card);
+    for (let i = 0; i < drawCount.value; i += 1) {
+      const card = deal();
+      player2Hand.push(card);
+    }
     // Append the card element to the card container
+    scoreDiff -= highLow(player2Hand);
+
+    const cardElement = printHand(player2Hand);
     cardContainer.appendChild(cardElement);
 
     playersTurn = 1;
-
-    if (player1Card.rank > player2Card.rank) {
+    drawCount.disabled = false;
+    if (scoreDiff > 0) {
       output('Player 1 wins!');
-    } else if (player1Card.rank < player2Card.rank) {
+    } else if (scoreDiff < 0) {
       output('Player 2 wins!');
     } else {
       output('Tie!');
     }
   }
+};
+
+const printHand = (playerHand) => {
+  const hand = document.createElement('div');
+  for (let i = 0; i < playerHand.length; i += 1) {
+    hand.appendChild(printCard(playerHand[i]));
+  }
+  return hand;
 };
 
 const printCard = (cardInfo) => {
@@ -120,6 +145,11 @@ const printCard = (cardInfo) => {
 
   const card = document.createElement('div');
   card.classList.add('card');
+  console.log(cardInfo);
+  if (cardInfo.highLow) {
+    console.log('here');
+    card.classList.add('high-low');
+  }
 
   card.appendChild(name);
   card.appendChild(suit);
@@ -128,15 +158,36 @@ const printCard = (cardInfo) => {
 };
 
 const initGame = () => {
+  playersTurn = 1;
+  scoreDiff = 0;
   makeDeck();
+
+  const player1Button = document.createElement('button');
+  player1Button.className = 'button';
+  const player2Button = document.createElement('button');
+  player2Button.className = 'button';
+
+  gameInfo.className = 'output';
+
+  cardContainer.classList.add('card-container');
+  document.body.appendChild(cardContainer);
 
   document.body.appendChild(gameInfo);
   // initialize button functionality
+  const buttonDiv = document.createElement('div');
   player1Button.innerText = 'Player 1 Draw';
-  document.body.appendChild(player1Button);
+  buttonDiv.appendChild(player1Button);
 
   player2Button.innerText = 'Player 2 Draw';
-  document.body.appendChild(player2Button);
+  buttonDiv.appendChild(player2Button);
+  document.body.appendChild(buttonDiv);
+
+  drawCount.type = 'number';
+  drawCount.min = '2';
+  drawCount.max = '5';
+  drawCount.value = 2;
+  drawCount.style = 'text-align:center';
+  document.body.appendChild(drawCount);
 
   player1Button.addEventListener('click', player1Click);
   player2Button.addEventListener('click', player2Click);
